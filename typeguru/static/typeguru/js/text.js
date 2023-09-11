fd32526df7f840e436bae0f7b553ad1a7a307a4d
TheJazzDev <babsman4all@gmail.com>
2023-09-11 03:22:48 +0100

    setting up docker

---
+++ typeguru/static/typeguru/js/text.js
@@ -0,0 +1,19 @@
+// fetch('/api/user-info')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching user info:', error);
+//   });
+
+let content = `
+  <p class="text-[10rem] text-justify">
+  ask change there stand will home from again over would part should want just that number up person during develop have because much state large this another all take change take line
+  </p>
+`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#content-section').innerHTML+=content;
+});
